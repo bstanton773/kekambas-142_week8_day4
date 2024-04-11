78e2860cccdccc94f52a452c05fd8fe4118dcd58
Brian <bstanton773@gmail.com>
2024-04-11 16:35:01 -0500

    Render the components on the canvas

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,21 @@ const firstComponent = new Component();
 console.log(firstComponent);
 console.log(firstComponent.shape);
 console.log(firstComponent.shape.attributes);
+
+firstComponent.height = 4;
+firstComponent.width = 4;
+firstComponent.locationLeft = 3;
+
+firstComponent.shape.backgroundColor = 'red';
+firstComponent.shape.borderStyle = 'dashed';
+firstComponent.shape.borderWidth = '5px';
+
+
+canvas.addComponent(firstComponent);
+
+const secondComponent = new Component();
+secondComponent.locationLeft = 4;
+secondComponent.locationTop = 2;
+secondComponent.shape.zIndex = 1;
+
+canvas.addComponent(secondComponent);
