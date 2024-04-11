38dec0f0826afde2599e4a1d2ef2cf0dacf2e1c2
Brian <bstanton773@gmail.com>
2024-04-11 13:03:50 -0500

    Initial commit with webpack, TS, and live-server

---
+++ dist/bundle.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+console.log('Hello World!');
+console.log('Log another line');
+function sayHello(name) {
+    console.log('Hello ' + name + '!');
+}
+sayHello('Brian');
+sayHello('Travis');
+sayHello('Kevin');
+console.log('Making a change');
+
+})();
+
+/******/ })()
+;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+console.log('Log another line');
+
+function sayHello(name:string){
+    console.log('Hello ' + name + '!')
+}
+
+sayHello('Brian');
+sayHello('Travis');
+sayHello('Kevin');
+
+console.log('Making a change');
